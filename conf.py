6c0b42c23f6ab92189475b3ac485a2eb0e44bbac
Erich Geiger <18083548+ergg@users.noreply.github.com>
2023-03-11 19:10:05 +0100

    New

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'alma_vagrant_ansible'
+project = 'alma_vagrant'
 copyright = '2023, Erich Geiger'
 author = 'Erich Geiger'
 release = '1.0'
@@ -31,4 +31,3 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath('../')) # path relative to conf.py
 # sys.path.insert(0, os.path.abspath('/home/erich/script/vagrant_root/alma_linux/playbook/' )) # path relative to conf.py
-
