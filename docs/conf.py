d4d94351cb55719b4abf02cdad4204d62a176cf3
ergg <18083548+ergg@users.noreply.github.com>
2023-03-12 20:07:31 +0100

    Update conf.py
    making new conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,21 +14,34 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
 import sys
 import os
 sys.path.insert(0, os.path.abspath('../')) # path relative to conf.py
-# sys.path.insert(0, os.path.abspath('/home/erich/script/vagrant_root/alma_linux/playbook/' )) # path relative to conf.py
-
